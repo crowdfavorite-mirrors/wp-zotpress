bbb3b3fbd868e6176a804f5c2f2397684148778f
Jonathan Johnson <jon@crowdfavorite.com>
2013-11-12 14:51:39 -0700

    modify widget wordpress paths

--- lib/widget/widget.ckeditor.php
+++ lib/widget/widget.ckeditor.php
@@ -3,7 +3,7 @@
     if (isset($_GET['iframe']) && trim($_GET['iframe']) == "true")
     {
         // Include WordPress
-        require('../../../wp-load.php');
+        require('../../../wp/wp-load.php');
         
         global $wpdb;
         
